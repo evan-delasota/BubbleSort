697d405dba63e4b8b39dd6782d4697a8dde1bc45
Evan D <Evan_delasota@yahoo.com>
2021-01-09 04:10:14 +0000

    Added big(O) comments

--- Main.java
+++ Main.java
@@ -17,10 +17,10 @@ class Main {
     System.out.println();
 
   }
-
+  // On average O(n^2) time complexity, O(1) space complexity
   static void bSort(int[] arr) {
     int length = arr.length;
-
+    
     for (int i = 0; i < length - 1; ++i) {
       for (int j = 0; j < length - i - 1; ++j) {
         if (arr[j] > arr[j + 1]) {
